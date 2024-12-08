58e4699243f6de97b256df5407d0a3c69d1c284b
r-saraswat <ridhimasaraswat012@gmail.com>
2024-12-08 15:38:50 +0530

    running version1

--- constants/index.ts
+++ constants/index.ts
@@ -3,16 +3,28 @@ export const headerLinks = [
     label: 'Home',
     route: '/',
   },
+  {label: 'Create Event',
+   route: '/events/create',
+  },
   {
-    label: 'Create Event',
-    route: '/events/create',
+    label: 'Events',
+    route: '/events',
+    submenu: [
+      {
+        label: 'Upcoming Events',
+        route: '/events/upcoming',
+      },
+      {
+        label: 'Past Events',
+        route: '/events/past',
+      },
+    ],
   },
   {
     label: 'My Profile',
     route: '/profile',
   },
-]
-
+];
 export const eventDefaultValues = {
   title: '',
   description: '',
