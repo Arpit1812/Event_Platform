a077d9fd63de323efb50c3f369f841057b173ce6
Arpit1812 <arpitdogra033@gmail.com>
2024-12-20 14:15:52 +0530

    events page editing

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,10 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: 'Evently',
-  description: 'Evently is a platform for event management.',
+  title: 'Spotlight',
+  description: 'Spotlight is a platform for event management.',
   icons: {
-    icon: '/assets/images/logo.svg'
+    icon: '/assets/images/Spotlight-Logo-High-Resolution.png'
   }
 }
 
