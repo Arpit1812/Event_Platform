ec71f35f50da7390d79adc9f9e327be884507fa9
Arpit1812 <arpitdogra033@gmail.com>
2024-12-20 16:14:46 +0530

    events/id updated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,70 +1,3 @@
-// import CategoryFilter from '@/components/shared/CategoryFilter';
-// import Collection from '@/components/shared/Collection'
-// import Search from '@/components/shared/Search';
-// import { Button } from '@/components/ui/button'
-// import { getAllEvents } from '@/lib/actions/event.actions';
-// import { SearchParamProps } from '@/types';
-// import Image from 'next/image'
-// import Link from 'next/link'
-
-// export default async function Home({ searchParams }: SearchParamProps) {
-//   const page = Number(searchParams?.page) || 1;
-//   const searchText = (searchParams?.query as string) || '';
-//   const category = (searchParams?.category as string) || '';
-
-//   const events = await getAllEvents({
-//     query: searchText,
-//     category,
-//     page,
-//     limit: 6
-//   })
-
-//   return (
-//     <>
-//       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
-//         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
-//           <div className="flex flex-col justify-center gap-8">
-//             <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
-//             <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
-//             <Button size="lg" asChild className="button w-full sm:w-fit">
-//               <Link href="#events">
-//                 Explore Now
-//               </Link>
-//             </Button>
-//           </div>
-
-//           <Image 
-//             src="/assets/images/hero.png"
-//             alt="hero"
-//             width={1000}
-//             height={1000}
-//             className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
-//           />
-//         </div>
-//       </section> 
-
-//       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-//         <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
-
-//         <div className="flex w-full flex-col gap-5 md:flex-row">
-//           <Search />
-//           <CategoryFilter />
-//         </div>
-
-//         <Collection 
-//           data={events?.data}
-//           emptyTitle="No Events Found"
-//           emptyStateSubtext="Come back later"
-//           collectionType="All_Events"
-//           limit={6}
-//           page={page}
-//           totalPages={events?.totalPages}
-//         />
-//       </section>
-//     </>
-//   )
-// }
-
 
 import CategoryFilter from '@/components/shared/CategoryFilter';
 import Collection from '@/components/shared/Collection';
@@ -114,16 +47,11 @@ export default async function Home({ searchParams }: SearchParamProps) {
           <div className="flex items-center gap-8 lg:gap-32.5">
             <div className="relative mx-auto hidden aspect-[588/526.5] md:block md:w-1/2">
               <Image
-                src="/assets/images/process-home.png"
+                src="/assets/images/Home-photo-collage2.png"
                 alt="About"
                 className="dark:hidden"
-                fill
-              />
-              <Image
-                src="/images/about/about-dark-01.png"
-                alt="About"
-                className="hidden dark:block"
-                fill
+                width={550}
+                height={300}
               />
             </div>
 
